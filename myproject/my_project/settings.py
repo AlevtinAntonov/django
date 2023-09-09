54e8cce667660010558e893c387b59df29ec3b96
Alevtin_linux_home <antonov3017@gmail.com>
2023-09-09 14:02:29 +0300

    myapp5 added

--- myproject/my_project/settings.py
+++ myproject/my_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'myapp2',
     'myapp3',
     'myapp4',
+    'myapp5',
     'hw_1_app',
     'hw_2_app',
     'hw_3_app',
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
